7b4f6af459c7217388052bfa6fb98ad4835dc8e2
osanwe <osanwe@protonmail.ch>
2015-07-09 17:40:44 +0400

    Corrected database initialization

--- qml/js/storage.js
+++ qml/js/storage.js
@@ -28,8 +28,8 @@ function getDatabase() {
 function initDatabase() {
     console.log("initDatabase()")
     var db = getDatabase()
-    if (db.version !== "3") {
-        db.changeVersion("2", "3", function(tx) {
+    if (db.version !== "" && db.version !== "3") {
+        db.changeVersion(db.version, "3", function(tx) {
             console.log("... recreate tables")
             tx.executeSql("DROP TABLE settings")
         })
