898a0a2883c4005ff8fe46ce3495274faa4e3c20
Albert Shteiner <ckazzku@gmail.com>
2015-09-13 12:54:37 +0500

    Flashing unread message indicator

--- src/notificationhelper.cpp
+++ src/notificationhelper.cpp
@@ -1,4 +1,5 @@
 #include "notificationhelper.h"
+#include <QtDBus>
 
 NotificationHelper::NotificationHelper(QObject *parent) :
     QObject(parent)
@@ -12,6 +13,18 @@ void NotificationHelper::sendNotification(const QString &body, const QString &su
 
     notification.publish();
     startTimer(3000);
+}
+
+void NotificationHelper::activateLed()
+{
+    QDBusMessage message = QDBusMessage::createMethodCall(
+                "com.nokia.mce",
+                "/com/nokia/mce/request",
+                "com.nokia.mce.request",
+                "req_led_pattern_activate");
+    message.setArguments(QList<QVariant>() << "PatternCommunicationIM");
+    QDBusConnection connection(QDBusConnection::systemBus());
+    connection.send(message);
 }
 
 void NotificationHelper::timerEvent(QTimerEvent *event)
