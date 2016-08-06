84181ca421846e495b335919bf83d5b283cfb574
Petr Vytovtov <osanwe@protonmail.ch>
2016-08-06 23:20:31 +0400

    Lisesing

--- src/vk/apirequest.h
+++ src/vk/apirequest.h
@@ -32,7 +32,28 @@ public:
     enum TaskType {
         FRIENDS_GET,
         FRIENDS_GET_MUTUAL,
-        FRIENDS_GET_ONLINE,
+        FRIENDS_GET_ONLINE/*
+  Copyright (C) 2016 Petr Vytovtov
+  Contact: Petr Vytovtov <osanwe@protonmail.ch>
+  All rights reserved.
+
+  This file is part of Kat.
+
+  Kat is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Kat is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Kat.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+,
         MESSAGES_GET_CHAT,
         MESSAGES_GET_DIALOGS,
         USERS_GET,
--- src/vk/users.cpp
+++ src/vk/users.cpp
@@ -1,3 +1,24 @@
+/*
+  Copyright (C) 2016 Petr Vytovtov
+  Contact: Petr Vytovtov <osanwe@protonmail.ch>
+  All rights reserved.
+
+  This file is part of Kat.
+
+  Kat is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Kat is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Kat.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "users.h"
 
 Users::Users(QObject *parent) : QObject(parent)
@@ -39,18 +60,23 @@ void Users::get(QStringList ids) {
 void Users::gotResponse(QJsonValue value, ApiRequest::TaskType type) {
     QJsonArray users;
     switch (type) {
-    case ApiRequest::USERS_GET:
+    case ApiRequest::USERS_GET: {
         users = value.toArray();
         if (users.size() == 1) emit gotUserProfile(User::fromJsonObject(users.at(0).toObject()));
         break;
+    }
 
-    case ApiRequest::USERS_GET_FRIENDS:
+    case ApiRequest::USERS_GET_FRIENDS: {
         users = value.toArray();
         QList<QObject*> usersList;
         for (int index = 0; index < users.size(); ++index) {
             usersList.append(Friend::fromJsonObject(users.at(index).toObject()));
         }
         emit gotUsersList(usersList);
+        break;
+    }
+
+    default:
         break;
     }
 }
