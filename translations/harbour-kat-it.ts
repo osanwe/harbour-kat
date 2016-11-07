aa3b8118a4f6570aef9ee130eac9a4ff6bafc7cf
Fra <fravaccaro90@gmail.com>
2016-11-07 19:19:14 +0100

    Italian translation

---
+++ translations/harbour-kat-it.ts
@@ -0,0 +1,296 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="26"/>
+        <source>The unofficial client for vk.com, distributed under the terms of the GNU GPLv3.</source>
+        <translation>Client non ufficiale per Vk, distribuito sotto licenza GNU GPLv3.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="27"/>
+        <source>You can contact or help with links listed below:</source>
+        <translation>Puoi contattare o contribuire tramite i seguenti link:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="28"/>
+        <source>VK page</source>
+        <translation>Pagina Vk</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="29"/>
+        <source>GitHub</source>
+        <translation>GitHub</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="30"/>
+        <source>Donate</source>
+        <translation>Dona</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="31"/>
+        <source>Ósanwe</source>
+        <translation>Ósanwe</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="32"/>
+        <source>Nostariel</source>
+        <translation>Nostariel</translation>
+    </message>
+</context>
+<context>
+    <name>AudioPlayerPage</name>
+    <message>
+        <location filename="../qml/pages/AudioPlayerPage.qml" line="16"/>
+        <source>Search audio...</source>
+        <translation>Cerca audio...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AudioPlayerPage.qml" line="37"/>
+        <source>My audios</source>
+        <translation>I miei audio</translation>
+    </message>
+</context>
+<context>
+    <name>DialogPage</name>
+    <message>
+        <location filename="../qml/pages/DialogPage.qml" line="89"/>
+        <location filename="../qml/pages/DialogPage.qml" line="90"/>
+        <source>Your message</source>
+        <translation>Messaggio</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DialogPage.qml" line="163"/>
+        <source>Photo</source>
+        <translation>Foto</translation>
+    </message>
+</context>
+<context>
+    <name>DialogsListPage</name>
+    <message>
+        <location filename="../qml/pages/DialogsListPage.qml" line="45"/>
+        <source>Dialogs</source>
+        <translation>Conversazioni</translation>
+    </message>
+</context>
+<context>
+    <name>FriendsListPage</name>
+    <message>
+        <location filename="../qml/pages/FriendsListPage.qml" line="40"/>
+        <source>Friends</source>
+        <translation>Amici</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FriendsListPage.qml" line="40"/>
+        <source>Online friends</source>
+        <translation>Amici in linea</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FriendsListPage.qml" line="41"/>
+        <source>Mutual friends</source>
+        <translation>Amici in comune</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="51"/>
+        <source>Logged to vk.com with Kat</source>
+        <translation>Loggato a vk.com da Kat</translation>
+    </message>
+</context>
+<context>
+    <name>MainMenuPage</name>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="29"/>
+        <source>My profile</source>
+        <translation>Il mio profilo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="30"/>
+        <source>News</source>
+        <translation>Notizie</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="31"/>
+        <source>Messages</source>
+        <translation>Messaggi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="32"/>
+        <source>Friends</source>
+        <translation>Amici</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="89"/>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainMenuPage.qml" line="94"/>
+        <source>Logout</source>
+        <translation>Logout</translation>
+    </message>
+</context>
+<context>
+    <name>NewsView</name>
+    <message>
+        <location filename="../qml/views/NewsView.qml" line="68"/>
+        <source>[Wall post] </source>
+        <translation>[Post in bacheca]</translation>
+    </message>
+</context>
+<context>
+    <name>NewsfeedPage</name>
+    <message>
+        <location filename="../qml/pages/NewsfeedPage.qml" line="43"/>
+        <source>Newsfeed</source>
+        <translation>Bacheca</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NewsfeedPage.qml" line="99"/>
+        <source>Like</source>
+        <translation>Mi piace</translation>
+    </message>
+</context>
+<context>
+    <name>ProfilePage</name>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="32"/>
+        <source>Photos</source>
+        <translation>Foto</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="33"/>
+        <source>Videos</source>
+        <translation>Video</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="34"/>
+        <source>Audios</source>
+        <translation>Audio</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="35"/>
+        <source>Groups</source>
+        <translation>Gruppi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="36"/>
+        <source>Pages</source>
+        <translation>Pagine</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="37"/>
+        <source>Followers</source>
+        <translation>Seguaci</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="38"/>
+        <source>Friends</source>
+        <translation>Amici</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="39"/>
+        <source>Online friends</source>
+        <translation>Amici in linea</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="40"/>
+        <source>Mutual Friends</source>
+        <translation>Amici in comune</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="41"/>
+        <source>Notes</source>
+        <translation>Note</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="59"/>
+        <source>Go to dialog</source>
+        <translation>Vai alla conversazione</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="183"/>
+        <source>Gender:</source>
+        <translation>Genere:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="187"/>
+        <source>Female</source>
+        <translation>Donna</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="187"/>
+        <source>Male</source>
+        <translation>Uomo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="198"/>
+        <source>Birthday:</source>
+        <translation>Compleanno:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="213"/>
+        <source>City:</source>
+        <translation>Città:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="230"/>
+        <source>Single</source>
+        <translation>Single</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="233"/>
+        <source>In a relationship</source>
+        <translation>Impegnato/a</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="236"/>
+        <source>Engaged</source>
+        <translation>Fidanzato/a ufficialmente</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="239"/>
+        <source>Married</source>
+        <translation>Sposato/a</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="242"/>
+        <source>It&apos;s complicated</source>
+        <translation>In una relazione complicata</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="245"/>
+        <source>Actively searching</source>
+        <translation>Alla ricerca</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ProfilePage.qml" line="248"/>
+        <source>In love</source>
+        <translation>Innamorato/a</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayerPage</name>
+    <message>
+        <location filename="../qml/pages/VideoPlayerPage.qml" line="54"/>
+        <source>The video is loading...</source>
+        <translation>Video in caricamento...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/VideoPlayerPage.qml" line="91"/>
+        <source>The video is opened in a browser</source>
+        <translation>Video aperto nel browser</translation>
+    </message>
+</context>
+<context>
+    <name>WallPostPage</name>
+    <message>
+        <location filename="../qml/pages/WallPostPage.qml" line="40"/>
+        <source>Like</source>
+        <translation>Mi piace</translation>
+    </message>
+</context>
+</TS>
