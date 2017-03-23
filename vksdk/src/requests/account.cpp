135186dcf31dd3d6fc830465de55ab9122c8e497
Nikita Baryshnikov <nib952051@gmail.com>
2017-03-23 17:10:39 +0300

    Account port to makeApiGetRequest

--- vksdk/src/requests/account.cpp
+++ vksdk/src/requests/account.cpp
@@ -11,19 +11,19 @@ void Account::setApi(ApiRequest *api) {
 }
 
 void Account::setOnline() {
-    QUrlQuery *query = new QUrlQuery();
-    query->addQueryItem("voip", QString::number(0));
+    QUrlQuery query;
+    query.addQueryItem("voip", "0");
     _api->makeApiGetRequest("account.setOnline", query, ApiRequest::ACCOUNT_SET_ONLINE);
 }
 
 void Account::banUser(int id) {
-    QUrlQuery *query = new QUrlQuery();
-    query->addQueryItem("user_id", QString::number(id));
+    QUrlQuery query;
+    query.addQueryItem("user_id", QString::number(id));
     _api->makeApiGetRequest("account.banUser", query, ApiRequest::ACCOUNT_BAN_USER);
 }
 
 void Account::unbanUser(int id) {
-    QUrlQuery *query = new QUrlQuery();
-    query->addQueryItem("user_id", QString::number(id));
+    QUrlQuery query;
+    query.addQueryItem("user_id", QString::number(id));
     _api->makeApiGetRequest("account.unbanUser", query, ApiRequest::ACCOUNT_UNBAN_USER);
 }
